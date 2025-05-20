7f64fd4e99e5759122ec25c06e0a4d0948a3d4d5
root <root@Bala-Jarvis>
2025-05-20 15:00:43 +0530

    Jenkins_update

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -7,4 +7,4 @@ if sudo docker ps -a --format '{{.Names}}' | grep "${env}"
 then
 sudo docker stop ${env} && sudo docker rm ${env}
 fi
-sudo docker run -it -d -p 9000:8001 --name ${env} balu361988/busapp:${version}
+sudo docker run -it -d -p 6000:8001 --name ${env} balu361988/busapp:${version}
